4dfd81d471366d679b7576dfcc611acddd57daf7
SamanSepahvand <shantav75@gmail.com>
2020-10-18 19:45:47 +0330

    splash screen

--- app/src/main/java/com/samansepahvand/myapplication/SplashScreen.java
+++ app/src/main/java/com/samansepahvand/myapplication/SplashScreen.java
@@ -15,7 +15,6 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
