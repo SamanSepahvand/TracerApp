5969587e0d0ddc57ebb1ffdc65532dd492db39c3
SamanSepahvand <shantav75@gmail.com>
2020-10-18 19:44:08 +0330

    splash screen

---
+++ app/src/main/java/com/samansepahvand/myapplication/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.samansepahvand.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MapsActivity.class));
+                finish();
+            }
+        },3000);
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+
+
+}
